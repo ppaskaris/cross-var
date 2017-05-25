71e51691edd954000b5e72fd66b360af5e0ebc5b
Patrick Paskaris <Patrick.Paskaris@D2L.com>
2017-05-25 16:29:04 -0400

    Remove babel

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-#!/usr/bin/env node --harmony
+'use strict';
 
-import spawn from "cross-spawn";
-import os from "os";
-import { exec } from "child_process";
-import exit from 'exit';
+const spawn = require("cross-spawn");
+const os = require("os");
+const exec = require("child_process").exec;
+const exit = require('exit');
 
-function normalize( args, isWindows ) {
-    return args.map( arg => {
-        Object.keys( process.env ).forEach( key => {
-            const regex = new RegExp( `\\$${ key }|%${ key }%`, "i" );
-            arg = arg.replace( regex, process.env[ key ] );
-        } );
+function normalize(args, isWindows) {
+    return args.map(arg => {
+        Object.keys(process.env).forEach(key => {
+            const regex = new RegExp(`\\$${key}|%${key}%`, "i");
+            arg = arg.replace(regex, process.env[key]);
+        });
         return arg;
-    } )
+    })
 }
 
-let args = process.argv.slice( 2 );
-if ( args.length === 1 ) {
-    const [ command ] = normalize( args );
-    const proc = exec( command, ( error, stdout, stderr ) => {
-        if ( error ) {
-            console.error( `exec error: ${ error }` );
+let args = process.argv.slice(2);
+if (args.length === 1) {
+    const [command] = normalize(args);
+    const proc = exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
             return;
         }
-        process.stdout.write( stdout );
-        process.stderr.write( stderr );
+        process.stdout.write(stdout);
+        process.stderr.write(stderr);
         exit(proc.code);
     });
 } else {
-    args = normalize( args );
+    args = normalize(args);
     const command = args.shift();
-    const proc = spawn.sync( command, args, { stdio: "inherit" } );
+    const proc = spawn.sync(command, args, { stdio: "inherit" });
     exit(proc.status);
 }
